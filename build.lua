10a1872f26d8609533d466474b4b2da0d5194ff5
Rultor.com <gpg@rultor.com>
2025-12-24 11:47:21 +0000

    version set to 0.23.0

--- build.lua
+++ build.lua
@@ -21,7 +21,7 @@ checkruns = 1
 
 uploadconfig = {
   pkg = "eolang",
-  version = "0.0.0",
+  version = "0.23.0",
   author = "Yegor Bugayenko",
   uploader = "Yegor Bugayenko",
   email = "yegor256@gmail.com",
@@ -42,7 +42,7 @@ uploadconfig = {
 function update_tag(file, content, tagname, tagdate)
   return string.gsub(
     string.gsub(content, "0%.0%.0", tagname),
-    "0000/00/00", os.date("%Y/%m/%d")
+    "2025/12/24", os.date("%Y/%m/%d")
   )
 end
 
