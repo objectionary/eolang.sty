094c78af8ef1d3b49c1ab80802fad9ad82c1cdc4
Rultor.com <gpg@rultor.com>
2026-06-30 16:20:51 +0000

    version set to 0.23.1

--- build.lua
+++ build.lua
@@ -21,7 +21,7 @@ checkruns = 1
 
 uploadconfig = {
   pkg = "eolang",
-  version = "0.0.0 0000-00-00",
+  version = "0.23.1 2026-06-30",
   author = "Yegor Bugayenko",
   uploader = "Yegor Bugayenko",
   email = "yegor256@gmail.com",
